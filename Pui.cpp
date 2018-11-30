655098cbf763275c8538348287ab80980ef380fa
Anton Pusch <anton.pusch@outlook.de>
2018-11-30 19:21:19 +0100

    more buttons

--- Pui.cpp
+++ Pui.cpp
@@ -5,14 +5,26 @@
 *********************************************************************/
 Pui::Pui() {}
 
-/*
-KeyListener Pui::decreasePage() { return k[0]; }
-KeyListener Pui::increasePage() { return k[1]; }
-*/
-
-KeyListener Pui::start()      { return k[0]; }
-KeyListener Pui::stop()       { return k[1]; }
-KeyListener Pui::selectMenu() { return k[2]; }
+KeyListener Pui::decreasePage()         { return k[0];  }
+KeyListener Pui::increasePage()         { return k[1];  }
+KeyListener Pui::selectPage()           { return k[2];  }
+KeyListener Pui::resetProperties()      { return k[3];  }
+KeyListener Pui::selectMenu()           { return k[4];  }
+KeyListener Pui::kick()                 { return k[5];  }
+KeyListener Pui::compass()              { return k[6];  }
+KeyListener Pui::animation()            { return k[7];  }
+KeyListener Pui::lineCalibration()      { return k[8];  }
+KeyListener Pui::lightBeamCalibration() { return k[9];  }
+KeyListener Pui::start()                { return k[10]; }
+KeyListener Pui::stop()                 { return k[11]; }
+KeyListener Pui::record()               { return k[12]; }
+KeyListener Pui::headstart()            { return k[13]; }
+KeyListener Pui::motor()                { return k[14]; }
+KeyListener Pui::bluetooth()            { return k[15]; }
+KeyListener Pui::kicker()               { return k[16]; }
+KeyListener Pui::bottom()               { return k[17]; }
+KeyListener Pui::debug()                { return k[18]; }
+KeyListener Pui::silent()               { return k[19]; }
 
 void Pui::update() {
     for(int i = 0; i < NUMBER_OF_KEYS; i++) k[i].update();
--- Pui.h
+++ Pui.h
@@ -3,39 +3,55 @@
 
 #include "Core.h"
 
-#define NUMBER_OF_KEYS 3
+#define NUMBER_OF_KEYS 20
 
 class Pui
 {
   public:
     Pui();
+    KeyListener decreasePage();
+    KeyListener increasePage();
+    KeyListener selectPage();
+    KeyListener resetProperties();
+    KeyListener selectMenu();
+    KeyListener kick();
+    KeyListener compass();
+    KeyListener animation();
+    KeyListener lineCalibration();
+    KeyListener lightBeamCalibration();
     KeyListener start();
     KeyListener stop();
-    KeyListener selectMenu();
+    KeyListener record();
+    KeyListener headstart();
+    KeyListener motor();
+    KeyListener bluetooth();
+    KeyListener kicker();
+    KeyListener bottom();
+    KeyListener debug();
+    KeyListener silent();
     void update();
   private:
-    KeyListener k[3] = {
-      /*KeyListener(PUI_STOP, 0, 500, 200), // decreasePage
-      KeyListener(PUI_STOP, 0, 500, 200), // increasePage
-      KeyListener(PUI_STOP, 0),    // selectPage
-      KeyListener(PUI_STOP, 2000),    // resetProperties (left + right)
-      KeyListener(PUI_STOP, 0),    // selectMenu
-      KeyListener(PUI_STOP, 0, 1000, 0),    // kick
-      KeyListener(PUI_STOP, 0, 0, 0),    // compass
-      KeyListener(PUI_STOP, 0),    // animation
-      KeyListener(PUI_STOP, 500),    // lineCalibration
-      KeyListener(PUI_STOP, 500),    // lightBeamCalibration
-      KeyListener(PUI_STOP, 0),    // headstart
-      KeyListener(PUI_STOP, 0),    // motor
-      KeyListener(PUI_STOP, 0),    // bluetooth
-      KeyListener(PUI_STOP, 0),    // kicker
-      KeyListener(PUI_STOP, 0),    // bottom
-      KeyListener(PUI_STOP, 0),    // debug
-      KeyListener(PUI_STOP, 0),    // silent
-      KeyListener(PUI_STOP, 600, 0, 0),    // flat*/
-      KeyListener(PUI_START, 0, 0, 0),          // start
-      KeyListener(PUI_STOP, 0, 0, 0),           // stop
-      KeyListener(PUI_ROT_BTN, 500, 2000, 100)  // select
+    KeyListener k[20] = {
+      KeyListener(PUI_ROT_BTN, 0, 500, 200), // decreasePage
+      KeyListener(PUI_ROT_BTN, 0, 500, 200), // increasePage
+      KeyListener(PUI_ROT_BTN, 0),           // selectPage
+      KeyListener(PUI_ROT_BTN, 2000),        // resetProperties (left + right)
+      KeyListener(PUI_ROT_BTN, 0),           // selectMenu
+      KeyListener(PUI_ROT_BTN, 0, 1000, 0),  // kick
+      KeyListener(PUI_ROT_BTN, 0, 0, 0),     // compass
+      KeyListener(PUI_ROT_BTN, 0),           // animation
+      KeyListener(PUI_ROT_BTN, 500),         // lineCalibration
+      KeyListener(PUI_ROT_BTN, 500),         // lightBeamCalibration
+      KeyListener(PUI_START, 0, 0, 0),       // start
+      KeyListener(PUI_STOP, 0, 0, 0),        // stop
+      KeyListener(PUI_ROT_BTN, 0, 0, 0),     // record (start + stop)
+      KeyListener(PUI_ROT_BTN, 0),           // headstart (lever)
+      KeyListener(PUI_ROT_BTN, 0),           // motor     (lever)
+      KeyListener(PUI_ROT_BTN, 0),           // bluetooth (lever)
+      KeyListener(PUI_ROT_BTN, 0),           // kicker    (lever)
+      KeyListener(PUI_ROT_BTN, 0),           // bottom    (lever)
+      KeyListener(PUI_ROT_BTN, 0),           // debug     (lever)
+      KeyListener(PUI_ROT_BTN, 0)            // silent    (lever)
     };
 };
 #endif
