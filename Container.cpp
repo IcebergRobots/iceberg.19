911cd1a080caecd9c935989812cd19d3ceb46b47
Anton Pusch <anton.pusch@outlook.de>
2019-03-15 17:15:30 +0100

    beautify container

--- Container.cpp
+++ Container.cpp
@@ -1,15 +1,24 @@
 #include "Container.h"
 
 /*********************************************************************
-  Constructor
-  add this object to the container
+  Konstruktor
+  - füge dieses Objekt zum Container hinzu
 *********************************************************************/
 Container::Container() {
     getObjects(this);
 }
 
+/*********************************************************************
+  greife auf den Container zu
+  @param myself:
+    1) NULL
+      - Objektcontainer zurückgeben
+      - gib eine Liste aus Pointern (konvertiert zu Integern) zu allen Objekten dieser Klasse zurück
+    2) Pointer zum eigenen Objekt
+      - füge das eigene Objekt zum Container hinzu
+*********************************************************************/
 LinkedList<int> *Container::getObjects(Container *myself) {
-    static LinkedList<int> objectAddresses;
-    if (myself == NULL) return &objectAddresses;
-    else objectAddresses.add((int)myself);
+    static LinkedList<int> objectAddresses; // statischer klassenweite Liste alle Objektpointer
+    if (myself == NULL) return &objectAddresses; // gebe Containerliste zurück, wenn keine Parameter angegeben sind
+    else objectAddresses.add((int)myself); // füge das eigene Objekt zum Container hinzu
 }
