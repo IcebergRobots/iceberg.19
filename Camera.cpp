a934657417bb3c11092ecd952d9a0e4e2b478f58
Anton Pusch <anton.pusch@outlook.de>
2019-03-15 16:39:10 +0100

    beautify camera

--- Camera.cpp
+++ Camera.cpp
@@ -3,76 +3,89 @@
 /*********************************************************************
   Constructor
 *********************************************************************/
-Camera::Camera() {
-    setLocked(30);
-    setCooldown(100);
+Camera::Camera()
+{
+  setLocked(30);  // begrenze die Abfragehäufigkeit, um die Framerate der Kamera nicht zu überschreiten
+  setCooldown(100); // die Kamera muss nicht so häufig aktualisiert werden
 }
 
 /*********************************************************************
   initialisiert die SPI Kommunikation
 *********************************************************************/
-void Camera::init() { 
-    if (isEnabled()) {
-        beginSegment(F("c"));
-        SPI.begin();
-        Pixy::init();
-        io.cameraResponse.set(SPI.transfer(0x00) == 255);
-        endSegment();
-    } else debug(F("-c"));
+void Camera::init()
+{
+  if (isEnabled())
+  {
+    beginSegment(F("c"));
+    SPI.begin(); // initialisiere den SPI Bus
+    Pixy::init();
+    io.cameraResponse.set(SPI.transfer(0x00) == 255); // erkennen, ob die Kamera angeschlossen ist
+    endSegment();
+  }
+  else
+    debug(F("-c"));
 }
 
-void Camera::frame() {
-    if (isEnabled()) {
-        beginSegment(F("c:r"));
-        if (io.turbo.on()) setLED(0, 0, 0); // schalte die Front-LED aus
-        int ballAreaMax = 0;  // Ballgröße, 0: blind, >0: Flächeninhalt
-        int goalAreaMax = 0;  // Torgröße,  0: blind, >0: Flächeninhalt
-        int eastHeightMax = 0;  // Farbmarkierungsgröße,  0: blind, >0: Flächeninhalt
-        int westHeightMax = 0;  // Farbmarkierungsgröße,  0: blind, >0: Flächeninhalt
+void Camera::frame()
+{
+  if (isEnabled())
+  {
+    beginSegment(F("c:r"));
+    if (io.turbo.on())
+      setLED(0, 0, 0);     // schalte die Front-LED aus
+    int ballAreaMax = 0;   // Ballgröße, 0: blind, >0: Flächeninhalt
+    int goalAreaMax = 0;   // Torgröße,  0: blind, >0: Flächeninhalt
+    int eastHeightMax = 0; // Farbmarkierungsgröße,  0: blind, >0: Flächeninhalt
+    int westHeightMax = 0; // Farbmarkierungsgröße,  0: blind, >0: Flächeninhalt
 
-        blockCount = getBlocks();
-        blockCountBall = 0;
-        blockCountGoal = 0;
-        // Liest alle Blöcke aus und zählt diese
-        // Sendet "cs error" über USB bei Fehler in Prüfsumme eines empfangenen Objekts
+    blockCount = getBlocks();
+    blockCountBall = 0;
+    blockCountGoal = 0;
+    // Liest alle Blöcke aus und zählt diese
+    // Sendet "cs error" über USB bei Fehler in Prüfsumme eines empfangenen Objekts
 
-        for (byte i = 0; i < blockCount; i++) { // geht alle erkannten Bloecke durch
-            int height = blocks[i].height;
-            int width = blocks[i].width;
-            int x = blocks[i].x - X_CENTER;
-            int signature = blocks[i].signature;
-            int angle = blocks[i].angle;
-            int area = height * width;
-            switch (signature) { // Was sehe ich?
-                case SIGNATURE_BALL:
-                    blockCountBall++;
-                    if (area > ballAreaMax) {
-                        ballAreaMax = area;
-                        io.ball.set(x);           // merke Ballwinkel
-                        io.ballWidth.set(width);  // merke Ballbreite
-                        io.ballArea.set(area);    // merke Ballgröße
-                        io.seeBall.set();
-                        io.closeBall.set(width > BALL_WIDTH_TRIGGER);
-                    }
-                break;
-                case SIGNATURE_GOAL:
-                    blockCountGoal++;
-                    if (area > goalAreaMax) {
-                        goalAreaMax = area;
-                        io.goal.set(x);           // merke Torwinkel
-                        io.goalWidth.set(width);  // merke Torbreite
-                        io.goalArea.set(area);    // merke Torgröße
-                        io.seeGoal.set();
-                    }
-                break;
-            }
+    for (byte i = 0; i < blockCount; i++)
+    { // geht alle erkannten Bloecke durch
+      int height = blocks[i].height;  // Blockhöhe
+      int width = blocks[i].width;  // Blockbreite
+      int x = blocks[i].x - X_CENTER; // Horizontaler Blockrichtung
+      int signature = blocks[i].signature; // Blocktyp
+      int angle = blocks[i].angle;  // Drehwinkel von Colorcodes
+      int area = height * width;  // Blockgröße
+      switch (signature)
+      { // Was sehe ich?
+      case SIGNATURE_BALL:
+        blockCountBall++;
+        if (area > ballAreaMax) // Der neue Block ist relevanter und überschreibt
+        {
+          ballAreaMax = area;
+          io.ball.set(x);          // merke Ballwinkel
+          io.ballWidth.set(width); // merke Ballbreite
+          io.ballArea.set(area);   // merke Ballgröße
+          io.seeBall.set();        // merke Sichtungszeit des Balls
+          io.closeBall.set(width > BALL_WIDTH_TRIGGER); // merke ggf. Sichtungszeit eines nahen Balls
         }
-        endSegment();
+        break;
+      case SIGNATURE_GOAL:
+        blockCountGoal++;
+        if (area > goalAreaMax) // Der neue Block ist relevanter und überschreibt
+        {
+          goalAreaMax = area;
+          io.goal.set(x);          // merke Torwinkel
+          io.goalWidth.set(width); // merke Torbreite
+          io.goalArea.set(area);   // merke Torgröße
+          io.seeGoal.set();        // merke Sichtungszeit des Tors
+        }
+        break;
+      }
     }
+    endSegment();
+  }
 }
 
-bool Camera::isEnabled() {
-    return CAMERA_ENABLED;
+bool Camera::isEnabled()
+{
+  return CAMERA_ENABLED;
 }
 
 Camera camera;
