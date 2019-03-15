16cb77f10ae88785524c8b5f35793fd1b3a8d37d
Anton Pusch <anton.pusch@outlook.de>
2019-03-15 23:09:50 +0100

    clean up

--- iceberg.19.ino
+++ iceberg.19.ino
@@ -63,44 +63,16 @@ void loop()
     EEPROM.write(1, abs(io.headingOffset.get())); // speichere Winkel
   }
   if (io.animation.click())
-  {
     io.animationEnabled.set(!io.animationEnabled.get());
-  }
   if (io.lineCalibration.click())
-  {
     BOTTOM_SERIAL.write(42);
-  }
   if (io.ballTouchCalibration.click())
-  {
     reflexion.calibrate();
-  }
   if (io.start.click())
-  {
     io.pause.set(false);
-  }
   if (io.stop.click())
-  {
     io.pause.set(true);
-  }
 
-  if (io.record.click())
-  {
-    debug(F("record"));
-  }
-  if (io.resetProperties.click())
-  {
-    debug(F("resetProperties"));
-  }
-  if (io.kickerStart.click())
-  {
-    debug(F("kickerStart"));
-    io.kickPermanent.set(true);
-  }
-  if (io.kickerStop.click())
-  {
-    debug(F("kickerStop"));
-    io.kickPermanent.set(false);
-  }
   if (io.shiftStart.click())
   {
     debug(format(us.left(), 3, 3));
@@ -113,64 +85,6 @@ void loop()
   }
   if (io.shiftStop.click())
   {
-    io.indRight.setLimits();
-    io.indRight.startDebug();
-
-    scan();
-    debug("set 1:");
-    io.indRight.set(1);
-    scan();
-
-    debug("set 1:");
-    io.indRight.set(1);
-    scan();
-
-    debug("update:");
-    io.indRight.update();
-    scan();
-
-    debug("set 2:");
-    io.indRight.set(2);
-    scan();
-
-    debug("set 1:");
-    io.indRight.set(1);
-    scan();
-
-    debug("update:");
-    io.indRight.update();
-    scan();
-
-    debug("set -1:");
-    io.indRight.set(-1);
-    scan();
-
-    debug("update:");
-    io.indRight.update();
-    scan();
-
-    debug("set 3:");
-    io.indRight.set(3);
-    scan();
-
-    debug("update:");
-    io.indRight.update();
-    scan();
-  }
-
-  if (io.record.click())
-    debug(F("record"));
-  if (io.resetProperties.click())
-    debug(F("resetProperties"));
-  if (io.kickerStart.click())
-  {
-    debug(F("kickerStart"));
-    io.kickPermanent.set(true);
-  }
-  if (io.kickerStop.click())
-  {
-    debug(F("kickerStop"));
-    io.kickPermanent.set(false);
   }
 
   updateStates();
